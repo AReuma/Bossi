6ef6e05ab84d49d280b472f3a151df990ee46c37
AReuma <kuuniin@gmail.com>
2023-03-10 23:07:34 +0900

    init: set project

---
+++ backend/bossi/src/main/java/com/example/bossi/controller/helloController.java
@@ -0,0 +1,17 @@
+package com.example.bossi.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/hello")
+public class helloController {
+
+    @GetMapping("/")
+    public void Hello(){
+        log.info("할루!");
+    }
+}
